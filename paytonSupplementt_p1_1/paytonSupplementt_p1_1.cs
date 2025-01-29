864e10a3e7ffbaee316f6249f553385c70a1908b
donp8on <paytonadonis@gmail.com>
2025-01-29 13:26:20 -0500

    initial commit

---
+++ paytonSupplementt_p1_1/paytonSupplementt_p1_1.cs
@@ -0,0 +1,9 @@
+﻿namespace paytonSupplementt_p1_1
+{
+    public class paytonSupplementt_p1_1
+    {
+        public static double AreaOfRect(double len, double wid) {
+            throw new NotImplementedException();
+        }
+    }
+} 

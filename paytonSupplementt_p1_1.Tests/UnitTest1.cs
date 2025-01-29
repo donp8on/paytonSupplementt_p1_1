b9bd6191a9d7a08cb5ae14785895981578d61cab
donp8on <paytonadonis@gmail.com>
2025-01-29 14:33:22 -0500

    feat: implemented AreaOfRect for base case

--- paytonSupplementt_p1_1.Tests/UnitTest1.cs
+++ paytonSupplementt_p1_1.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace paytonSupplementt_p1_1.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace paytonSupplementt_p1_1.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldCalcAreaOfRectangeWithDifferentLenAndWid()
+    {
+        Assert.True(50.0 == paytonSupplementt_p1_1.AreaOfRect(5.0, 10.0));
     }
 }
--- paytonSupplementt_p1_1/paytonSupplementt_p1_1.cs
+++ paytonSupplementt_p1_1/paytonSupplementt_p1_1.cs
@@ -1,20 +1,22 @@
-﻿namespace paytonSupplementt_p1_1
+﻿namespace paytonSupplementt_p1_1;
+
+public class paytonSupplementt_p1_1
 {
-    public class paytonSupplementt_p1_1
+    public static double AreaOfRect(double len, double wid)
     {
-        public static double AreaOfRect(double len, double wid) {
-            throw new NotImplementedException();
-        }
-
-        public static double AreaOfCircle(double radius)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static double AreaOfTriangle(double triBase, double triHeight)
-        {
-            throw new NotImplementedException();
-        }
-
-
+        return len * wid;
     }
+
+    public static double AreaOfCircle(double radius)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static double AreaOfTriangle(double triBase, double triHeight)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
+

336d5692c5adea983789e90b14d8639d8871e977
donp8on <paytonadonis@gmail.com>
2025-01-29 14:35:13 -0500

    test: added test for rectangle area with same length and width

--- paytonSupplementt_p1_1.Tests/UnitTest1.cs
+++ paytonSupplementt_p1_1.Tests/UnitTest1.cs
@@ -7,4 +7,10 @@ public class UnitTest1
     {
         Assert.True(50.0 == paytonSupplementt_p1_1.AreaOfRect(5.0, 10.0));
     }
+
+    [Fact]
+    public void ShouldCalcAreaOfRectangeWithSameLenAndWid()
+    {
+        Assert.True(25.0 == paytonSupplementt_p1_1.AreaOfRect(5.0, 5.0));
+    }
 }
